9c9bd92679e19e55a97176ac9603bd7416b3af33
tieuquyngok1995 <tieuquyngok1995@gmail.com>
2020-03-16 12:18:52 +0900

    add src

---
+++ demo/src/test/java/spring/boot/example/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.boot.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
